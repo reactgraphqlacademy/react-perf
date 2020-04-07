e73b726f7a885561084bc60d252555c65b30b7c5
alexlbr <alex.lobera@gmail.com>
2020-04-07 07:45:57 +0100

    refactor Todo component from class to function

--- src/exercise_3/Todo.jsx
+++ src/exercise_3/Todo.jsx
@@ -1,45 +1,43 @@
 import React from "react";
 
-class Todo extends React.PureComponent {
-  render() {
-    const {
-      removeUserFromTodo,
-      data: { text, users, id }
-    } = this.props;
+const Todo = props => {
+  const {
+    removeUserFromTodo,
+    data: { text, users, id }
+  } = props;
 
-    return (
-      <li className="media" style={{ borderBottom: "1px solid #ccc" }}>
-        <div className="media-left" />
-        <div className="media-body">
-          <h4 className="media-heading">{text}</h4>
-          <ul className="list-unstyled list-inline" style={{ float: "right" }}>
-            {users.map(user => (
-              <li key={user.id}>
-                <a
-                  onClick={() =>
-                    removeUserFromTodo({ userId: user.id, todoId: id })
-                  }
-                >
-                  <img
-                    className="img-circle"
-                    title={`By ${user.name}`}
-                    style={{
-                      width: "50px",
-                      height: "50px",
-                      float: "right"
-                    }}
-                    src={user.imageUrl}
-                  />
-                  <br />
-                  Remove
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </li>
-    );
-  }
-}
+  return (
+    <li className="media" style={{ borderBottom: "1px solid #ccc" }}>
+      <div className="media-left" />
+      <div className="media-body">
+        <h4 className="media-heading">{text}</h4>
+        <ul className="list-unstyled list-inline" style={{ float: "right" }}>
+          {users.map(user => (
+            <li key={user.id}>
+              <a
+                onClick={() =>
+                  removeUserFromTodo({ userId: user.id, todoId: id })
+                }
+              >
+                <img
+                  className="img-circle"
+                  title={`By ${user.name}`}
+                  style={{
+                    width: "50px",
+                    height: "50px",
+                    float: "right"
+                  }}
+                  src={user.imageUrl}
+                />
+                <br />
+                Remove
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </li>
+  );
+};
 
-export default Todo;
+export default React.memo(Todo);
